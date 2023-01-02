74f3a1d74ae6c5817d3c344a907db2494a16d870
Nagendra <gadinagendra333@gmail.com>
2023-01-02 18:14:33 +0530

    Second commit by User B

--- src/Class1.java
+++ src/Class1.java
@@ -5,6 +5,13 @@ public class Class1 {
 		// TODO Auto-generated method stub
 
 		System.out.println("Class 1");
+		
+	
+		
+		for (int i =0 ; i<10; i++)
+		{
+			System.out.println(i);
+		}
 	}
 
 }
---
+++ src/Class_2.java
@@ -0,0 +1,11 @@
+
+public class Class_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Class 2");
+	}
+
+}
